2ce8a22773839c0d290e06a86891d7affeefbc54
zyongan <yonganzhong@outlook.com>
2020-04-23 17:23:30 +0800

    add compare_input_readline and strip test

--- python_base/console_input_output.py
+++ python_base/console_input_output.py
@@ -21,6 +21,7 @@ def input_multi_line():
         # for line in sys.stdin
         while True:
             line = sys.stdin.readline().strip()
+            # if line == ''
             if not line:
                 break
             line = list(map(int, line.split()))
@@ -29,6 +30,7 @@ def input_multi_line():
     except Exception as e:
         print(e)
         pass
+
 
 def input_fixed_line():
     """
@@ -49,6 +51,52 @@ def input_fixed_line():
         pass
 
 
+def compare_input_readline():
+    """
+    input 只读取字符串，自动去掉换行符(\n)
+    readline: 读取整行信息，包括换行符(\n)
+
+    :return:
+    """
+    m = input()
+    n = sys.stdin.readline()
+    print(len(m))
+    print(len(n))
+    assert m == n.strip(), "m != n"
+
+
+def strip_test():
+    """
+    strip: 移除字符串头尾指定的字符，当chars==None时，移除首尾的 ’ ‘|’\n‘|’\t‘
+           Note: 只能删除首位，不能删除中间数据
+    :return:
+    """
+    m = "good"
+    n = " good\n"
+    q = "good\t\n"
+
+    # print raw length
+    print("Now print raw length")
+    print(len(m))
+    print(len(n))
+    print(len(q))
+
+    # execute strip
+    n = n.strip()
+    q = n.strip()
+
+    assert m == n and n == q, "m, n, q"
+    # print update length
+    print('Following print length by strip')
+    print(len(m))
+    print(len(n))
+    print(len(q))
+
+
 if __name__ == "__main__":
 
-    input_multi_line()
+    # compare_input_readline()
+    # m = " falgafab\n\t"
+    # print(len(m))
+    # print(len(m.strip()))
+    strip_test()
