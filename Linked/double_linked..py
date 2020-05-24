d18b1e5380dbb8e21ec68aeef16e5dc50dd1a390
zyongan <yonganzhong@outlook.com>
2020-05-24 16:56:47 +0800

    update double linked list

--- Linked/double_linked..py
+++ Linked/double_linked..py
@@ -49,6 +49,61 @@ def insert_tail(head, item):
     return head
 
 
+def insert(head, index, item):
+    """
+    insert anywhere of index
+    if index low 0 then insert to head(0 index)
+    if index out of range index of linked then insert to tail
+
+    :param head:
+    :param index:
+    :param value:
+    :return:
+    """
+    # insert to head
+    if head is None or index <= 0:
+        head = DoubleLinkedNode(item, next=head)
+        head.next.prev = head
+    else:
+        probe = head
+        # curse to position of index-1
+        # or the last position
+        while index > 1 and probe.next != None:
+            probe = probe.next
+            index -= 1
+        # insert new node after node at position index-1
+        probe.next = DoubleLinkedNode(item, prev=probe, next=probe.next)
+
+    return head
+
+
+def delete(head, index):
+    """
+    delete anywhere of index
+    if index low 0 then insert to head(0 index)
+    if index out of range index of linked then insert to tail
+    :param head:
+    :param index:
+    :param item:
+    :return:
+    """
+    assert head is not None
+    if index <= 0:
+        head = head.next
+        head.prev = None
+    else:
+        probe = head
+        # curse to position of index-1
+        # or the next to last position
+        while index > 1 and probe.next.next != None:
+            probe = probe.next
+            index -= 1
+        probe.next = probe.next.next
+        probe.next.prev = probe
+
+    return head
+
+
 def traversal_linked(head, is_reverse=False):
     """
     traversal linked list
@@ -85,4 +140,17 @@ if __name__ == "__main__":
     print('traversal by reverse')
     traversal_linked(head, is_reverse=True)
 
+    print('The inserted index is beyond the chain table boundary')
+    head = insert(head, item='a', index=-1)
+    traversal_linked(head)
+    print('The inserted index is inner the chain table boundary')
+    head = insert(head, item='b', index=2)
+    traversal_linked(head)
+
+    print('The delete index is beyond the chain table boundary ')
+    head = delete(head, index=-1)
+    traversal_linked(head)
+    print('The delete index is inner the chain table boundary')
+    head = delete(head, index=1)
+    traversal_linked(head)
 
--- Linked/node.py
+++ Linked/node.py
@@ -8,3 +8,10 @@ class Node(object):
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
+
+class DoubleLinkedNode(object):
+    def __init__(self, data, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
