0b21f213ecea7ab170420f6512f3ad5c8f86422c
zyongan <yonganzhong@outlook.com>
2020-07-07 09:07:51 +0800

    add float  operation

--- c_basic/c_basic.cpp
+++ c_basic/c_basic.cpp
@@ -10,17 +10,21 @@
 int main(){
     // define constant method 2
     const int MAX_NUM_1 = 20;
-    printf("MAX_NUM_0 = %d \n", MAX_NUM_0);
-    printf("MAX_NUM_1 = %d \n", MAX_NUM_1);
+    printf("MAX_NUM_0 = %d \n", MAX_NUM_0); // MAX_NUM_0 = 10
+    printf("MAX_NUM_1 = %d \n", MAX_NUM_1); // MAX_NUM_1 = 20
 
     // get size of int
-    printf("int size: %lu byte \n", sizeof(int));
+    printf("int size: %lu byte \n", sizeof(int)); // int size: 4 byte
 
     // get float info
-    printf("float size : %lu \n", sizeof(float));
-    printf("float min value: %u\n", FLT_MIN );
-    printf("float max value: %u\n", FLT_MAX );
-    printf("precise value: %d\n", FLT_DIG );
+    printf("float size : %lu \n", sizeof(float)); // float size : 4
+    printf("float min value: %u\n", FLT_MIN ); // float min value: 0
+    printf("float max value: %u\n", FLT_MAX ); // float max value: 3758096384
+    printf("precise value: %d\n", FLT_DIG ); // precise value: 6
+
+    // compare float or double
+    printf("Add f after decimal means to float: %d\n", sizeof(32.0f)); // Add f after decimal means to float: 4
+    printf("Only decimal means to double: %d\n", sizeof(32.0)); // Only decimal means to double: 8
     
     return 0;
 }
