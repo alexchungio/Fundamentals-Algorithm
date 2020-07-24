73748f30e0efa285115dbd24531d64a5a664cf6a
zyongan <yonganzhong@outlook.com>
2020-07-24 11:26:36 +0800

    update  pointer

--- c_basic/pointer.cpp
+++ c_basic/pointer.cpp
@@ -15,6 +15,9 @@ void pointer_start();
 int find_max_min_a(int *, int, int*, int*);
 int find_largest(int *, int);
 // int find_max_min_b(int *, int, int*, int*);
+int find_row_largest_a(int * ,  int , int );//
+int find_row_largest_b(int * ,  int , int );
+int find_col_largest_a(int * ,  int , int );//
 
 int main(){
 
@@ -34,6 +37,19 @@ int main(){
     // printf("min=%d, max=%d", *min_b, *max_b);
     // get largest value
     printf("largest value=%d\n", find_largest(a, n));
+
+    /*tow demension array*/
+    int b [2][3] = {2, 4, 3, 6, 1, 7};
+    int num_row = sizeof(b) / sizeof(b[0]);
+    int num_col = sizeof(b[0]) / sizeof(b[0][0]);
+    //get largest of row 
+    int dst_row = 0; 
+    printf("lagest value of %d row is %d\n", dst_row, find_row_largest_a(b[0], num_col, dst_row));
+    printf("lagest value of %d row is %d\n", dst_row, find_row_largest_b(b[0], num_col, dst_row));
+    //get largest of col
+    int dst_col = 1;
+    printf("lagest value of %d col is %d\n", dst_col, find_col_largest_a(b[0], num_row, dst_col));
+    printf("lagest value of %d col is %d\n", dst_col, find_col_largest_b(b[0], num_row, num_col, dst_col));
     
     return 0;
 }
@@ -96,4 +112,39 @@ int find_largest(int* a, int n){
         }
     }
     return *largest;
+}
+
+
+int find_row_largest_a(int * a ,  int num_col , int dst_row ){
+    int v_largest = *(a+ dst_row);
+    for (int i=0; i<num_col; i++){
+        if (*((a+dst_row)+i)>v_largest){
+            v_largest = *((a+dst_row)+i);
+        }
+    }
+    return v_largest;
+}
+
+int find_row_largest_b(int * a  ,  int num_col , int dst_row ){
+    int v_largest = *(a + dst_row);
+    int * p;
+    for ( p=a+dst_row*num_col; p<a+(dst_row+1)*num_col; p++){
+        if (*p > v_largest){
+            v_largest = *p;
+        }
+    }
+    return v_largest;
+}
+
+
+int find_col_largest_a(int * a ,  int num_row , int dst_col){
+    int v_largest = *(a+ dst_col);
+    
+    for(int i=0; i<num_row; i++){
+        if(*(a+num_row*i+dst_col+1)>v_largest){
+            v_largest = *(a+num_row*i+dst_col+1);
+        }
+    }
+    return v_largest;
+
 }
