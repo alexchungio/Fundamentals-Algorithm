814d285aabeab4afc31d7a51c6e7fa41b93a786a
zya <zya@meitu.com>
2022-09-07 21:18:27 +0800

    std::map demo

---
+++ cplusplus/test/map_demo.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+# include <map>
+# include <algorithm>
+
+
+using namespace std;
+
+int main()
+{
+    map<int, string> m_0;
+    m_0.insert(pair<int, string>(0, "alex"));
+    m_0.insert(pair<int, string>(1, "tim"));
+    
+    // travesal-1
+    map<int, string>::iterator iter;
+    for(iter=m_0.begin(); iter != m_0.end(); iter++)
+    {
+        cout << iter -> first << ":" <<iter->second << endl;
+    }
+
+    // travesal-2
+    for(auto & m: m_0)
+    {
+        cout << m.first << ":" << m.second << endl;
+    }
+
+    return 0;
+
+    // find key
+    map<int, string>::iterator iter_0 = m_0.find(1);
+    if (iter_0 != m_0.end())
+    {
+        cout << iter -> first << ":" <<iter->second << endl;
+    }
+
+    // erase
+    map<int, string>::iterator iter_1 = m_0.find(0);
+    if (iter_1 != m_0.end())
+    {
+        m_0.erase(iter_1);
+    }
+
+    // std::tie <=> std::map
+
+    return 0;
+}
