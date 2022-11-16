a4f28cbdd54e42d129742d116aa4de6be0a1f2ed
zya <yonganzhong@outlook.com>
2022-11-16 19:53:27 +0800

    add opencv demo

---
+++ cplusplus/test/cv_demo.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+
+int main(){
+    std::string img_path = TEST_DATA "sunflower.jpg";
+    cv::Mat bgr_img = cv::imread(TEST_DATA "sunflower.jpg");
+    cv::imshow("image", bgr_img);
+    cv::waitKey(0);
+    
+    return 0;
+}
