636a9b51e3c1a762b6baf23738894d483e562951
zya <yonganzhong@outlook.com>
2022-11-29 16:31:35 +0800

    upate unique_ptr

--- cplusplus/test/smart_pointer_demo.cpp
+++ cplusplus/test/smart_pointer_demo.cpp
@@ -10,23 +10,27 @@ int main()
 {
 
     // create and initial
-    std::unique_ptr<float> m(new float(5));
+    std::unique_ptr<int> m(new int(5));
     std::cout << *m << std::endl;
 
     
-    std::unique_ptr<float> m_0(nullptr); //create
-    m_0.reset(new float(7));  // assign
+    std::unique_ptr<int> m_0(nullptr); //create
+    m_0.reset(new int(7));  // assign
     std::cout << *m_0 << std::endl;
 
     // create arrray unique_ptr
-    std::unique_ptr<float[]> m_1(new float[]{1, 3, 5, 7, 9});
-    std::cout << m_1[0] << "|" << m_1[4] << std::endl;
+    std::unique_ptr<int []> m_1(new int[5]{1, 3, 5, 7, 9});
+    std::cout << m_1[0] << " |" << m_1[4] << std::endl;
+
+    // make unique (c++ 14)
+    std::unique_ptr<int> m_2 = std::make_unique<int>(0);
 
     // unique_ptr don't soupport copy
     // move: m will been release
-    std::unique_ptr<float> n(std::move(m));
+    std::unique_ptr<int> n(std::move(m));
     assert(m==nullptr);
     std::cout << *n << std::endl;
+
     
     return 0;
 }
