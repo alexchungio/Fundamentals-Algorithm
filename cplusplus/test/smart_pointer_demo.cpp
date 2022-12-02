c40a78b32aea678c54d837ab37258945f9bb7d1c
zya <yonganzhong@outlook.com>
2022-12-02 14:51:01 +0800

    shared_ptr and weak_ptr demo

--- cplusplus/test/smart_pointer_demo.cpp
+++ cplusplus/test/smart_pointer_demo.cpp
@@ -1,35 +1,101 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
 /*
 unique_ptr
 shared_ptr
+weak_ptr
 */
 
-int main()
+void unique_ptr_demo()
 {
-
     // create and initial
     std::unique_ptr<int> m(new int(5));
     std::cout << *m << std::endl;
 
-    
-    std::unique_ptr<int> m_0(nullptr); //create
-    m_0.reset(new int(7));  // assign
+    // create then assign
+    std::unique_ptr<int> m_0(nullptr); 
+    m_0.reset(new int(7));  
     std::cout << *m_0 << std::endl;
 
     // create arrray unique_ptr
     std::unique_ptr<int []> m_1(new int[5]{1, 3, 5, 7, 9});
     std::cout << m_1[0] << " |" << m_1[4] << std::endl;
 
-    // make unique (c++ 14)
-    std::unique_ptr<int> m_2 = std::make_unique<int>(0);
-
-    // unique_ptr don't soupport copy
-    // move: m will been release
-    std::unique_ptr<int> n(std::move(m));
+    // move: unique_ptr don't soupport copy
+    // explicit transfer ownership
+    std::unique_ptr<int> m_2(std::move(m));
     assert(m==nullptr);
-    std::cout << *n << std::endl;
+    std::cout << *m_2 << std::endl;  
+    
+    // make_unique
+    // std::unique_ptr<int> v_1 = std::make_unique<int>(3);
+    auto v_1 = std::make_unique<int>(3);
+    std::cout << * v_1.get() << std::endl;
+
+    // array
+    auto v_2 = std::make_unique<int []>(5);
+    std::cout << v_2.get()[0] << std::endl;  
+    
+    // vector-1d
+    auto v_3 = std::make_unique<std::vector<int>>(5, 2);
+    std::cout << v_3.get()->data()[0] << std::endl;
+
+    // vector-2d
+    auto v_4 = std::make_unique<std::vector<std::vector<int>>>(3, std::vector<int>(4, 3));
+    std::cout << v_4.get()->data()[0][0] << std::endl;
+
+    std::cout << "********************"<< std::endl;
+}
+
+void shared_ptr_demo()
+{
+    std::shared_ptr<int> v_0(new int(3));
+    std::cout << "v_0: " << *v_0 << " reference count: " << v_0.use_count()<< std::endl;
+
+    // copy
+    std::shared_ptr<int> v_1 = v_0;
+    std::cout << "v_1: " << *v_0 << " reference count: " << v_0.use_count() << std::endl;
+    assert(v_0 == v_1);
+
+    // release
+    v_1.reset();
+    std::cout << "v_1: " << " reference count: " << v_0.use_count() << std::endl;
+
+    // reset
+    v_0.reset();
+    std::cout << "v_0: " << " reference count: " << v_0.use_count() << std::endl;
+    
+    std::cout << "********************"<< std::endl;
+
+}
+
+void weak_ptr_demo()
+{
+    auto v_0 = std::make_shared<int>(2);
+    std::cout << "v_0: " << *v_0 << " reference count: " << v_0.use_count() << std::endl;
+
+    // weak_ptr -> shared_ptr
+    // don't affact the resource reference count
+    auto v_1 = std::weak_ptr<int>();
+    v_1 = v_0;
+    std::cout << "v_1: " << " reference count: " << v_0.use_count() << std::endl;
+
+    // in order to get object must convert to shared_ptr
+    // with lock to get shred_ptr
+    std::shared_ptr<int> v_2 = v_1.lock();
+    std::cout << "v_2: " << *v_2 << " reference count: " << v_0.use_count() << std::endl;
+    
+    std::cout << "********************"<< std::endl;
+}
+
+
+int main()
+{ 
+    unique_ptr_demo();
+    shared_ptr_demo();
+    weak_ptr_demo();
 
     
     return 0;
